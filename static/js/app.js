fe875978d07441f62abdb41042de12b9a7419a24
TanvirMahin24 <tanvirmahin24@gmail.com>
2021-01-24 14:14:52 +0600

    contact section added

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,20 @@
+// Contact form
+$(document).ready(function () {
+  $(".form-control").click(function () {
+    $(this).parent().addClass("label-animate");
+  });
+
+  $(window).click(function () {
+    if (!$(event.target).is(".form-control")) {
+      $(".form-control").each(function () {
+        if ($(this).val() == "") {
+          $(this).parent().removeClass("label-animate");
+        }
+      });
+    }
+  });
+});
+
 // Chart options
 const options = {
   chart: {
